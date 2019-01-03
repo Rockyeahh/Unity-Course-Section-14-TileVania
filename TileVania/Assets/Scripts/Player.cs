e7b416ecc726d62f5afec23ab5570b40c531d505
Soren's Coding Land <chaz.com@hotmail.co.uk>
2019-01-03 16:26:49 +0000

    Lecture 352 - Move Player Horizontally - Done.
    I didn't like this one as much and I struggled to focus on it. It took far too long due my lack of focus and digging down a rabbit hole pointlessly.

---
+++ TileVania/Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour {
+
+    [SerializeField] float runSpeed = 5f;
+
+    Rigidbody2D myRigidbody;
+
+	void Start () {
+        myRigidbody = GetComponent<Rigidbody2D>();
+	}
+
+    void Update()
+    {
+        Run();
+    }
+
+    private void Run()
+    {
+        float controlThrow = CrossPlatformInputManager.GetAxisRaw("Horizontal"); // Value is between -1 to +1;
+        Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, myRigidbody.velocity.y); // Just use the current rigidbody velocity of y.
+        myRigidbody.velocity = playerVelocity; // What Why???
+    }
+}
