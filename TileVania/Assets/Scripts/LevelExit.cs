d9de7fd954bf30391602f0f2ac3864bce1f4392e
Soren's Coding Land <chaz.com@hotmail.co.uk>
2019-01-23 17:52:44 +0000

    Lecture 381 - Level Exit Portal - Done.
    I learned a shortcut for creating a sprite game object but otherwise I had done all of this before but at least it's all done.
    Rick also added a slow motion effect in but it wasn't something I wanted so I left it out.

---
+++ TileVania/Assets/Scripts/LevelExit.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelExit : MonoBehaviour {
+
+    [SerializeField] float LevelLoadDelay = 2f;
+
+    void OnTriggerEnter2D(Collider2D collider2D) // Rick writes other instead. Maybe he doesn't care what Collider2D is called.
+    {
+        // start coroutine and name the IEnumerator
+        StartCoroutine(LoadNextLevel());
+    }
+
+    IEnumerator LoadNextLevel()
+    {
+        // yield with a delay
+        yield return new WaitForSecondsRealtime(LevelLoadDelay);
+
+        var currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1); // load next scene
+    }
+
+}
