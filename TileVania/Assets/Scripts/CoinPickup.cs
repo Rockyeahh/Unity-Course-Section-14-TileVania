89e8980ddd9b19a9ecf1e7bcbb28df7c68d83b1e
Soren's Coding Land <chaz.com@hotmail.co.uk>
2019-01-28 17:33:27 +0000

    Lecture 385 - Coin Pickups -
    We added the coin sprite and made it interactable and also made it destroy itself when the player collides with it.

---
+++ TileVania/Assets/Scripts/CoinPickup.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinPickup : MonoBehaviour {
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Destroy(gameObject);
+        // Add to score.
+        // Create/Play SFX.
+    }
+
+}
