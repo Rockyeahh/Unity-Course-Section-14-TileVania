96ef46477df0cb4f17e2c958f969330198e249a8
Soren's Coding Land <chaz.com@hotmail.co.uk>
2019-02-02 15:31:10 +0000

    Lecture 330(Should be 388) - Remembering Pickups - Done/Broken
    The aim was to make it so that the coin pickups don't re-spawn when you lose a life but that doesn't seem to work as well several other things in this game. The bugs just keep piling up and they aren't focused on this at the moment. Bringing in new students/customers is their priority and making new courses for the current customer/student fan base can spend on.

--- TileVania/Assets/Scripts/LevelExit.cs
+++ TileVania/Assets/Scripts/LevelExit.cs
@@ -9,6 +9,12 @@ public class LevelExit : MonoBehaviour {
 
     void OnTriggerEnter2D(Collider2D collider2D) // Rick writes other instead. Maybe he doesn't care what Collider2D is called.
     {
+        ScenePersist sp = FindObjectOfType<ScenePersist>(); // Nina code for solving a bug where data isn't persisting due tothe ScenePersist not being destroyed.
+
+        if (sp != null)
+        {
+            Destroy(sp.gameObject);
+        }
         // start coroutine and name the IEnumerator
         StartCoroutine(LoadNextLevel());
     }
@@ -19,6 +25,7 @@ public class LevelExit : MonoBehaviour {
         yield return new WaitForSecondsRealtime(LevelLoadDelay);
 
         var currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        //Destroy(FindObjectOfType<ScenePersist>()); // This may be useful for later. It came from the q&a in Remembering Pickups. Irrelevant now.
         SceneManager.LoadScene(currentSceneIndex + 1); // load next scene
     }
 
---
+++ TileVania/Assets/Scripts/ScenePersist.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScenePersist : MonoBehaviour {
+
+    int startingBuildIndex;
+
+    private void Awake()
+    {
+        int numScenePersist = FindObjectsOfType<ScenePersist>().Length;
+        if (numScenePersist < 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+        startingBuildIndex = SceneManager.GetActiveScene().buildIndex;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex; // Update will keep checking whereas start/startingBuildIndex will not.
+        if (currentSceneIndex != startingBuildIndex)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
